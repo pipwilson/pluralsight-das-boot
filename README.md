c50e7d39246547c13558749636a41217815a72a1
Phil Wilson <github@philwilson.org>
2017-01-09 10:39:52 +0000

    Commit code at end of course, including updated syntax for ShipwreckControllerWebIntegrationTest

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # pluralsight-das-boot
+
 The Pluralsight Getting Started with Spring Boot course: https://app.pluralsight.com/library/courses/spring-boot-first-application/table-of-contents
+
+Original code is in https://github.com/dlbunker/ps-spring-boot-resources
+
---
+++ src/test/java/com/boot/ShipwreckControllerWebIntegrationTest.java
@@ -0,0 +1,42 @@
+package com.boot;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+
+import java.io.IOException;
+
+import org.hamcrest.Matchers;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
+public class ShipwreckControllerWebIntegrationTest {
+	
+
+	@Test
+	public void testListAll() throws IOException {
+
+		
+		TestRestTemplate restTemplate = new TestRestTemplate();
+		ResponseEntity<String> response = restTemplate.getForEntity("http://localhost:8080/api/v1/shipwrecks", String.class);
+
+	    assertThat( response.getStatusCode() , equalTo(HttpStatus.OK));
+
+	    ObjectMapper objectMapper = new ObjectMapper();
+	    JsonNode responseJson = objectMapper.readTree(response.getBody());
+
+	    assertThat( responseJson.isMissingNode() , Matchers.is(false) );
+	    assertThat( responseJson.toString() , equalTo("[]") );
+		
+	}
+
+}
